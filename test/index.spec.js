4a76d8938c28656b34e80a9bfebf122bd64cfb7d
Kodi Ackerman <kodiackerman@msn.com>
2019-08-16 10:09:06 -0700

    amend: updated tests to use "node-tdd"

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,12 @@
-const path = require('path');
 const expect = require('chai').expect;
-const nockBack = require('nock').back;
+const { desc } = require('node-tdd');
 const DocumentType = require('aws-sdk').DynamoDB.DocumentClient;
 const index = require('./../src/index');
 
-describe('Testing index', () => {
+desc('Testing index', { useNock: true }, () => {
   let aws;
   before(() => {
     aws = index();
-    nockBack.setMode('record');
-    nockBack.fixtures = path.join(__dirname, '__cassettes');
   });
 
   it('Testing global configuration', () => {
@@ -21,33 +18,26 @@ describe('Testing index', () => {
     expect(aws.get('DynamoDB.Converter')).to.be.a('object');
   });
 
-  it('Testing Exception', (done) => {
-    nockBack('s3-putObject-fail.json', async (nockDone) => {
-      aws.call('s3:putObject', {}).catch((e) => {
-        expect(e.message).to.equal(
-          'There were 2 validation errors:\n* MissingRequiredParameter: '
-          + 'Missing required key \'Bucket\' in params\n* MissingRequiredParameter: '
-          + 'Missing required key \'Key\' in params'
-        );
-        nockDone();
-        done();
-      }).then(done.fail);
-    });
+  it('Testing Exception', async () => {
+    try {
+      await aws.call('s3:putObject', {});
+    } catch (e) {
+      expect(e.message).to.equal(
+        'There were 2 validation errors:\n* MissingRequiredParameter: '
+        + 'Missing required key \'Bucket\' in params\n* MissingRequiredParameter: '
+        + 'Missing required key \'Key\' in params'
+      );
+    }
   });
 
-  it('Testing Expected Exception', (done) => {
-    nockBack('s3-putObject-fail-expected.json', async (nockDone) => {
-      aws.call('s3:putObject', {}, { expectedErrorCodes: ['MultipleValidationErrors'] }).then((code) => {
-        expect(code).to.equal('MultipleValidationErrors');
-        nockDone();
-        done();
-      }).catch(done.fail);
-    });
+  it('Testing Expected Exception', async () => {
+    const code = await aws.call('s3:putObject', {}, { expectedErrorCodes: ['MultipleValidationErrors'] });
+    expect(code).to.equal('MultipleValidationErrors');
   });
 
-  it('Testing Exception with Logger', (done) => {
-    nockBack('s3-putObject-fail-with-logger.json', async (nockDone) => {
-      index({
+  it('Testing Exception with Logger', async () => {
+    try {
+      await index({
         logger: {
           error: (msg) => {
             expect(msg).to.deep.contain({
@@ -57,15 +47,13 @@ describe('Testing index', () => {
             });
           }
         }
-      }).call('s3:putObject', {}).catch((e) => {
-        expect(e.message).to.equal(
-          'There were 2 validation errors:\n* MissingRequiredParameter: '
-          + 'Missing required key \'Bucket\' in params\n* MissingRequiredParameter: '
-          + 'Missing required key \'Key\' in params'
-        );
-        nockDone();
-        done();
-      }).then(done.fail);
-    });
+      }).call('s3:putObject', {});
+    } catch (e) {
+      expect(e.message).to.equal(
+        'There were 2 validation errors:\n* MissingRequiredParameter: '
+        + 'Missing required key \'Bucket\' in params\n* MissingRequiredParameter: '
+        + 'Missing required key \'Key\' in params'
+      );
+    }
   });
 });
