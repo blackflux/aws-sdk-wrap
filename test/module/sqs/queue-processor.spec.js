cb345ec0d051ab3ac377140059f60d16f4f364fc
Lukas Siemon <github@blackflux.com>
2020-05-15 15:25:30 -0700

    amend: minor

--- test/module/sqs/queue-processor.spec.js
+++ test/module/sqs/queue-processor.spec.js
@@ -24,7 +24,25 @@ describe('Testing QueueProcessor', {
     });
     executor = (records) => new Promise((resolve, reject) => {
       processor.handler({
-        Records: records.map((r) => ({ body: JSON.stringify(r) }))
+        Records: records.map((r) => ({
+          messageId: '11d6ee51-4cc7-4302-9e22-7cd8afdaadf5',
+          receiptHandle: 'AQEBBX8nesZEXmkhsmZeyIE8iQAMig7qw...',
+          body: JSON.stringify(r),
+          attributes: {
+            ApproximateReceiveCount: '1',
+            SentTimestamp: String(Date.now()),
+            SequenceNumber: '18849496460467696128',
+            MessageGroupId: '1',
+            SenderId: 'AIDAIO23YVJENQZJOL4VO',
+            MessageDeduplicationId: '1',
+            ApproximateFirstReceiveTimestamp: '1573251510774'
+          },
+          messageAttributes: {},
+          md5OfBody: 'e4e68fb7bd0e697a0ae8f1bb342846b3',
+          eventSource: 'aws:sqs',
+          eventSourceARN: 'arn:aws:sqs:us-east-2:123456789012:fifo.fifo',
+          awsRegion: 'us-east-2'
+        }))
       }, {}, (err, resp) => {
         if (err !== null) {
           reject(err);
