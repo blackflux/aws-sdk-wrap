a681a87f48ab221c30bd12151ca16160ddfeb32b
Lukas Siemon <github@blackflux.com>
2020-05-16 13:17:32 -0700

    fix: checking retry kwargs, error now passed into cbs

--- src/module/sqs/errors.js
+++ src/module/sqs/errors.js
@@ -1,33 +1,38 @@
-const assert = require('assert');
+const Joi = require('joi-strict');
 
 class RetryError extends Error {
-  constructor({
-    maxFailureCount = 10,
-    maxAgeInSec = Number.MAX_SAFE_INTEGER,
-    delayInSec = 0,
-    onTemporaryFailure = () => [],
-    onPermanentFailure = ({
-      logger,
-      limits,
-      meta,
-      payload
-    }) => {
-      logger.warn(`Permanent Retry Failure\n${JSON.stringify({ limits, meta, payload })}`);
-      return [];
-    }
-  } = {}) {
+  constructor(kwargs = {}) {
     super();
     this.name = this.constructor.name;
 
-    assert(Number.isInteger(maxFailureCount) && maxFailureCount >= 0, 'maxFailureCount');
-    assert(Number.isInteger(maxAgeInSec) && maxAgeInSec >= 0, 'maxAgeInSec');
-    assert(
-      typeof delayInSec === 'function'
-      || (Number.isInteger(delayInSec) && delayInSec >= 0 && delayInSec <= 15 * 60),
-      'delayInSec'
-    );
-    assert(typeof onTemporaryFailure === 'function', 'onTemporaryFailure');
-    assert(typeof onPermanentFailure === 'function', 'onPermanentFailure');
+    Joi.assert(kwargs, Joi.object().keys({
+      maxFailureCount: Joi.number().integer().min(1).optional(),
+      maxAgeInSec: Joi.number().integer().min(1).optional(),
+      delayInSec: Joi.alternatives(
+        // eslint-disable-next-line newline-per-chained-call
+        Joi.number().integer().min(0).max(900),
+        Joi.function()
+      ).optional(),
+      onTemporaryFailure: Joi.function().optional(),
+      onPermanentFailure: Joi.function().optional()
+    }));
+
+    const {
+      maxFailureCount = 10,
+      maxAgeInSec = Number.MAX_SAFE_INTEGER,
+      delayInSec = 0,
+      onTemporaryFailure = () => [],
+      onPermanentFailure = ({
+        logger,
+        limits,
+        meta,
+        payload
+      }) => {
+        logger.warn(`Permanent Retry Failure\n${JSON.stringify({ limits, meta, payload })}`);
+        return [];
+      }
+    } = kwargs;
+
     this.maxFailureCount = maxFailureCount;
     this.maxAgeInSec = maxAgeInSec;
     this.delayInSec = delayInSec;
