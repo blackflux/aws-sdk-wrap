cbf9e3e7d444cdaf9ab0bfc74987e2f482400b2b
Lukas Siemon <github@blackflux.com>
2020-12-14 08:41:14 -0800

    amend: minor

--- src/module/s3.js
+++ src/module/s3.js
@@ -5,7 +5,7 @@ const Joi = require('joi-strict');
 
 const sleep = util.promisify(setTimeout);
 
-module.exports.S3 = ({
+module.exports = ({
   call,
   logger,
   backoffFunction = (count) => 30 * (count ** 2),
