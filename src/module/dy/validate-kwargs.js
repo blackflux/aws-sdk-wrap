95af6c65c4cc5e8d62d5d75f2672b00682f6557b
Ezequiel Alanis <ealanisg@gmail.com>
2020-12-15 20:36:34 -0800

    feat: add model schema validation

--- src/module/dy/validate-kwargs.js
+++ src/module/dy/validate-kwargs.js
@@ -4,40 +4,64 @@ const schema = Joi.object().keys({
   name: Joi.string(),
   attributes: Joi.object().pattern(
     Joi.string(),
-    // todo: must have exactly one value with partitionKey
-    // todo: must have zero or one value with partitionKey
     Joi.object().keys({
       type: Joi.string().valid('string', 'boolean', 'number', 'list', 'map', 'binary', 'set'),
       partitionKey: Joi.boolean().valid(true).optional(),
       sortKey: Joi.boolean().valid(true).optional()
     }).nand('partitionKey', 'sortKey')
-  ),
+  ).min(1).custom((v, h) => {
+    const propertyCount = {};
+    Object.values(v).forEach((o) => Object.keys(o).forEach((k) => {
+      if (propertyCount[k] === undefined) {
+        propertyCount[k] = 1;
+      } else {
+        propertyCount[k] += 1;
+      }
+    }));
+    if (propertyCount.partitionKey === undefined) {
+      return h.message('At least one partitionKey must be defined');
+    }
+    if (propertyCount.partitionKey > 1) {
+      return h.message('Duplicated partitionKey definition');
+    }
+    if (propertyCount.sortKey !== undefined && propertyCount.sortKey > 1) {
+      return h.message('Duplicated sortKey definition');
+    }
+    return v;
+  }),
   indices: Joi.object().pattern(
     Joi.string(),
     Joi.object().keys({
-      // todo: check that value is present in attributes
       partitionKey: Joi.string().optional(),
-      // todo: check that value is present in attributes
       sortKey: Joi.string().optional()
     }).or('partitionKey', 'sortKey')
-  ),
+  ).optional().min(1)
+    .custom((v, h) => {
+      const valid = Object.values(v)
+        .every((idx) => ([...new Set(Object.values(idx))].length === Object.keys(idx).length));
+      if (valid === false) {
+        return h.message('Can\'t use the same attribute as partitionKey and sortKey');
+      }
+      return v;
+    }),
   DocumentClient: Joi.object()
+}).custom((v, h) => {
+  const { attributes, indices } = v;
+  if (indices !== undefined) {
+    const valid = Object.values(indices)
+      .every((idx) => Object.keys(attributes)
+        .includes(Object.values(idx)[0]));
+    if (valid === false) {
+      return h.message('Indices values must match with defined attributes');
+    }
+  }
+  return v;
 });
 
 module.exports = (kwargs) => {
   const result = schema.validate(kwargs);
-
   if (result.error) {
     throw new Error(result.error);
   }
-
-  /*
-    if (Object.keys(indexes).length > 0) {
-    Object.entries(indexes).forEach(([_, att]) => {
-      Object.entries(att).filter(([k, _]) => k === 'partitionKey').map(([_, a]) => a)[0];
-      const idxAttrNameSk = Object.entries(att).filter(([k, _]) => k === 'sortKey').map(([_, a]) => a)[0];
-      partitionKey == idx
-   */
-
-  return result.value;
+  return result;
 };
