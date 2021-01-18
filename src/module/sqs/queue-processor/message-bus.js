d31648040d3933320f575caabd6c486bf4089815
Lukas Siemon <github@blackflux.com>
2021-01-18 13:18:50 -0800

    amend: minor

--- src/module/sqs/queue-processor/message-bus.js
+++ src/module/sqs/queue-processor/message-bus.js
@@ -9,7 +9,7 @@ module.exports = ({
 }) => {
   const tasks = [];
   const addRaw = (msgs, queueUrl) => {
-    tasks.push([msgs, queueUrl]);
+    tasks.push([[...msgs], queueUrl]);
   };
   return {
     addRaw,
