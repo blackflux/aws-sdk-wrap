714087e376b5f8103726eeef0d640ccdf75c86a9
Lukas Siemon <github@blackflux.com>
2021-01-18 10:51:39 -0800

    amend: refactor

--- src/module/sqs/queue-processor.js
+++ src/module/sqs/queue-processor.js
@@ -10,8 +10,9 @@ const loadSteps = require('./queue-processor/load-steps');
 const MessageBus = require('./queue-processor/message-bus');
 const StepBus = require('./queue-processor/step-bus');
 const DlqBus = require('./queue-processor/dlq-bus');
-const { metaKey, stripPayloadMeta } = require('./queue-processor/payload');
+const { metaKey } = require('./queue-processor/payload');
 const Digraph = require('./queue-processor/digraph');
+const processEvent = require('./queue-processor/process-event');
 
 module.exports = ({
   sendMessageBatch,
@@ -49,37 +50,7 @@ module.exports = ({
   };
 
   const handler = wrap(async (event) => {
-    const stepContexts = new Map();
-    const tasks = event.Records
-      .map((e) => {
-        const payload = JSON.parse(e.body);
-        assert(
-          payload instanceof Object && !Array.isArray(payload),
-          `Invalid Event Received: ${e.body}`
-        );
-        assert(
-          payload.name !== undefined,
-          'Received step event that is missing "name" property.'
-        );
-        const step = steps[payload.name];
-        assert(
-          step !== undefined,
-          `Invalid step provided: ${payload.name}`
-        );
-        if (!stepContexts.has(step)) {
-          stepContexts.set(step, Object.create(null));
-        }
-        return [payload, stripPayloadMeta(payload), e, step];
-      });
-
-    if (event.Records.length !== 1) {
-      const invalidSteps = Array.from(stepContexts)
-        .filter(([step]) => !step.isParallel)
-        .map(([step]) => step.name);
-      if (invalidSteps.length !== 0) {
-        throw new Error(`SQS mis-configured. Parallel processing not supported for: ${invalidSteps.join(', ')}`);
-      }
-    }
+    const { tasks, stepContexts } = processEvent({ event, steps });
 
     const stepBus = StepBus({ steps, messageBus });
     const dlqBus = DlqBus({
