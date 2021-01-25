e59cc407067d8f4c2cad5f4403000ce252882989
Lukas Siemon <github@blackflux.com>
2021-01-25 13:28:36 -0800

    amend: refactor

--- src/module/sqs/queue-processor/dlq-bus.js
+++ src/module/sqs/queue-processor/dlq-bus.js
@@ -1,27 +1,27 @@
-const LRU = require('lru-cache-ext');
-
 module.exports = ({
   queues, getDeadLetterQueueUrl, messageBus, globalPool
 }) => {
-  const dlqCache = new LRU({ maxAge: 60 * 1000 });
   const pending = [];
+  const url2dlqUrl = {};
   return {
     prepare: (msgs, step) => {
-      pending.push([step, msgs]);
+      pending.push([queues[step.queue], msgs]);
     },
     propagate: async () => {
       if (pending.length === 0) {
         return;
       }
-      const fns = pending.splice(0).map(([step, msgs]) => async () => {
-        const queueUrl = queues[step.queue];
-        const dlqUrl = await dlqCache.memoize(
-          queueUrl,
-          () => getDeadLetterQueueUrl(queueUrl)
-        );
-        messageBus.addRaw(msgs, dlqUrl);
-      });
+      const inProgress = pending.splice(0);
+      const queueUrls = [...new Set(inProgress.map(([url]) => url))];
+      const fns = queueUrls
+        .filter((url) => !(url in url2dlqUrl))
+        .map((url) => async () => {
+          url2dlqUrl[url] = await getDeadLetterQueueUrl(url);
+        });
       await globalPool(fns);
+      inProgress.forEach(([url, msgs]) => {
+        messageBus.addRaw(msgs, url2dlqUrl[url]);
+      });
     }
   };
 };
